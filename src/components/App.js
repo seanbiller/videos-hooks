410e8050559a58cd5559f891dd653bb22810a548
Sean Biller <seanmbiller@gmail.com>
2020-12-04 09:58:00 -0500

    on Input change completed

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "semantic-ui-css/semantic.min.css";
+import SearchBar from "./SearchBar";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="ui container">
+        <SearchBar />
+      </div>
+    );
+  }
+}
+
+export default App;
